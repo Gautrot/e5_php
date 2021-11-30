b5210d8ea711320953a28197ac3160569b7ed533
NoraSEDJAI <87068779+NoraSEDJAI@users.noreply.github.com>
2021-11-30 08:36:49 +0100

    Inscription + Modification du compte prof
    Inscription et modification du compte PROF avec ajout d'une matiere

--- manager/prof/ManaProf.php
+++ manager/prof/ManaProf.php
@@ -95,4 +95,54 @@ class ManaProf
         // Sinon, on affiche un message d'erreur
         throw new Exception('Erreur.');
     }
+    // Méthode de modification d'un prof
+
+    /**
+     * @throws Exception
+     */
+    public function modifProf(Professeur $prof)
+    {
+        // On appelle la base de données
+        $bdd = (new BDD)->getBase();
+        $req = $bdd->query('SELECT * FROM utilisateur');
+        $res = $req->fetchAll();
+        foreach ($res as $error) {
+            // Vérifie les conditions lors de la modification d'un étudiant.
+            // S'il y a une erreur, la fonction s'arrête.
+            switch ($error) {
+                case ($error['mail'] == $_POST['mail'] && $error['idUtilisateur'] != $_SESSION['user']['idUtilisateur']):
+                    throw new Exception('L\'adresse mél est déjà pris par un autre utilisateur.');
+                case ($error['telephone'] == $_POST['telephone'] && $error['idUtilisateur'] != $_SESSION['user']['idUtilisateur']):
+                    throw new Exception('Le numéro de téléphone est déjà pris par un autre utilisateur.');
+                case ($error['login'] == $_POST['login'] && $error['idUtilisateur'] != $_SESSION['user']['idUtilisateur']):
+                    throw new Exception('Le login est déjà pris par un autre utilisateur.');
+                case ($error['nom'] == $_POST['nom'] && $error['prenom'] == $_POST['prenom'] && $error['idUtilisateur'] != $_SESSION['user']['idUtilisateur']):
+                    throw new Exception('Le nom et prénom sont déjà pris par un autre utilisateur.');
+            }
+        }
+        $req = $bdd->prepare('UPDATE utilisateur SET nom = :nom, prenom = :prenom, dateNaissance = :dateNaissance, adresse = :adresse, telephone = :telephone, mail = :mail, login = :login, mdp = :mdp WHERE idUtilisateur = :idUtilisateur');
+        $req->execute([
+            'idUtilisateur' => $prof->getIdUtilisateur(),
+            'nom' => $prof->getNom(),
+            'prenom' => $prof->getPrenom(),
+            'dateNaissance' => $prof->getDateNaissance(),
+            'adresse' => $prof->getAdresse(),
+            'telephone' => $prof->getTelephone(),
+            'mail' => $prof->getMail(),
+            'login' => $prof->getLogin(),
+            'mdp' => $prof->getMdp()
+        ]);
+        $req = $bdd->prepare('SELECT idUtilisateur, login, mdp, statut, validUtilisateur FROM utilisateur WHERE idUtilisateur = :idUtilisateur');
+        $req->execute([
+            'idUtilisateur' => $prof->getIdUtilisateur()
+        ]);
+        $res2 = $req->fetch();
+        // S'il modifie avec succès l'étudiant, alors il envoie la session.
+        if ($res2) {
+            unset($_SESSION['erreur']);
+            return $_SESSION['user'] = $res2;
+        }
+        // Sinon, on affiche un message d'erreur
+        throw new Exception('Modification échouée !');
+    }
 }
---
+++ template/themes/template/inscr-prof.php
@@ -0,0 +1,149 @@
+<?php
+//session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <?php include_once '../../../include/head.php' ?>
+    <title>Inscription</title>
+</head>
+
+<body>
+<?php
+// Preloader
+include_once '../../../include/modal/preloader.php';
+// Header
+include_once '../../../include/header.php';
+include_once '../../../model/BDD.php';
+// Modal Inscription
+include_once '../../../include/modal/inscription.php';
+// Modal Mot de passe oublié
+include_once '../../../include/modal/mdp.php';
+// Modal Login
+include_once '../../../include/modal/login.php';
+include_once '../../../include/modal/connectionEleve.php';
+include_once '../../../include/modal/connectionParent.php';
+include_once '../../../include/modal/connectionProf.php';
+include_once '../../../include/modal/connectionAdmin.php';
+?>
+
+<!-- about -->
+<section class="page-title-section overlay">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <ul class="list-inline custom-breadcrumb">
+                    <li class="list-inline-item">
+                        <a class="h2 text-primary font-secondary" href="@@page-link">
+                            Inscription
+                        </a>
+                    </li>
+                    <li class="list-inline-item text-white h3 font-secondary @@nasted"></li>
+                </ul>
+                <p class="text-lighten">Inscrivez-vous sur le site en tant que professeur.</p>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /about -->
+
+<!-- table -->
+<section class="section">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="modal-body">
+                    <div class="login">
+                        <form method="POST" action="/e5_php/traitement/prof/inscription-prof-tr.php" style="width:100%">
+                            <div class="form-row">
+                                <div class="col">
+                                    <label for="nom">Nom</label>
+                                    <input type="text" class="form-control form-control-sm mb-3" id="nom" name="nom"
+                                           required
+                                           maxlength="40" placeholder="ex : Schuman">
+                                </div>
+                                <div class="col">
+                                    <label for="prenom">Prénom</label>
+                                    <input type="text" class="form-control form-control-sm mb-3" id="prenom"
+                                           name="prenom"
+                                           required maxlength="40" placeholder="ex : Robert">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="dateNaissance">Né.e le</label>
+                                <input type="date" class="form-control form-control-sm mb-3" id="dateNaissance"
+                                       name="dateNaissance" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="telephone">Numéro de Téléphone</label>
+                                <input type="text" class="form-control form-control-sm mb-3" id="telephone"
+                                       name="telephone" required maxlength="10" placeholder="ex : 0123456789">
+                            </div>
+                            <div class="form-group">
+                                <label for="adresse">Adresse</label>
+                                <input type="text" class="form-control form-control-sm mb-3" id="adresse" name="adresse"
+                                       required placeholder="ex : 123 rue Robert Schuman">
+                            </div>
+                            <div class="form-group">
+                                <label for="mail">Adresse Mail</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text" id="basic-addon1">@</span>
+                                    </div>
+                                    <input type="email" class="form-control form-control-sm" id="mail" name="mail"
+                                           required placeholder="ex : r.schuman@gmail.com">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="login">Login</label>
+                                <input type="text" class="form-control form-control-sm mb-3" id="login" name="login"
+                                       required maxlength="40" placeholder="ex : rSchuman93">
+                            </div>
+                            <div class="form-group">
+                                <label for="mdp">Mot de passe</label>
+                                <input type="password" class="form-control form-control-sm mb-3" id="mdp" name="mdp"
+                                       required>
+                            </div>
+                            <div class="form-group">
+                                <label for="metier">Matiere</label>
+                                <input type="text" class="form-control form-control-sm mb-3" id="matiere" name="matiere"
+                                       required placeholder="ex : Français">
+                            </div>
+                            <button type="submit" class="btn btn-primary">
+                                S'inscrire
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- /table -->
+
+<section class="section-sm bg-primary">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="text-center">
+                    <form method="POST" action="index.php">
+                        <button type="submit" class="btn btn-primary">
+                            Retour
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php
+// Footer
+include_once '../../../include/footer.php';
+// Script
+include_once '../../../include/script.php';
+?>
+
+</body>
+</html>
