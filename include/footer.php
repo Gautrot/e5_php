920c5743c354802df3682bff2d5429373094ba13
Gautrot <72071525+Gautrot@users.noreply.github.com>
2021-12-08 19:38:41 +0100

    beta - Retour du fichier footer.php

---
+++ include/footer.php
@@ -0,0 +1,27 @@
+<footer>
+    <!-- footer content -->
+    <div class="footer bg-footer section border-bottom">
+        <div class="container">
+            <div class="row">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2620.219803894918!2d2.4132023151878976!3d48.94930037929656!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66b9c6b0800f7%3A0xa35833adc1739e08!2sLyc%C3%A9e%20Robert%20Schuman!5e0!3m2!1sfr!2sfr!4v1615390933960!5m2!1sfr!2sfr"
+                        width="110%" height="400" style="border:50px;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+        </div>
+    </div>
+    <!-- copyright -->
+    <div class="copyright py-4 bg-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-7 text-sm-left text-center">
+                    <p class="mb-0">Copyright
+                        <script>
+                            var CurrentYear = new Date().getFullYear()
+                            document.write(CurrentYear)
+                        </script>
+                        © LPRS
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
